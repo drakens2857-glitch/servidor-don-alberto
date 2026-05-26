9bb367ce731044b387928970d9e628c58301921f
Nicolas Bel-084 <draken.s2857@gmail.com>
2026-05-26 15:42:43 +0000

    Primer commit: Estructura base GigaMoto

---
+++ app.py
@@ -0,0 +1,64 @@
+from flask import Flask, request, jsonify
+from datetime import datetime
+
+app = Flask(__name__)
+DATA_FILE = '/var/www/html/peritajes.json'
+
+# Inventario inventado
+registros = {
+    "servidor": "Ubuntu de Neira",
+    "hora": str(datetime.now()),
+    "inventario": [
+        {
+            "moto": "Ford Raptor",
+            "placa": "NSB308",
+            "estado": "Cmabio llantas"
+        },
+        {
+            "moto": "Kawasaki FZ900",
+            "placa": "NSB830",
+            "estado": "Mantenimiento"
+        }
+    ]
+}
+
+# Lista de peritajes
+peritajes = [
+    {
+        "placa": "NBS830"
+    }
+]
+
+# Ruta GET inventario (Dejamos SOLO 'GET' para evitar confusiones)
+@app.route('/api/registros', methods=['GET'])
+def ver_registros():
+    return jsonify(registros)
+
+# Ruta GET peritajes (Para ver las placas acumuladas)
+@app.route('/api/peritajes', methods=['GET'])
+def ver_peritajes():
+    return jsonify(peritajes)
+
+# Ruta POST peritajes (Esta es la que procesa y guarda la nueva placa)
+@app.route('/api/peritajes', methods=['POST'])
+def agregar_peritaje():
+    data = request.get_json()
+
+    # Validación por si mandas el body vacío en Postman
+    if not data or "placa" not in data:
+        return jsonify({"error": "Falta el campo 'placa' en la petición"}), 400
+
+    nueva_placa = {
+        "placa": data["placa"]
+    }
+
+    peritajes.append(nueva_placa)
+
+    return jsonify({
+        "mensaje": "Vehiculo registrado correctamente",
+        "datos": nueva_placa
+    }), 201
+
+if __name__ == '__main__':
+
+    app.run(host='0.0.0.0', port=5000)
